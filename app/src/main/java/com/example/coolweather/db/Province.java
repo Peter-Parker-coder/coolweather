e110d76d3b262c370e5a820a47905559702b1d82
Neo <Neo@gmail.com>
2020-05-30 17:31:33 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/example/coolweather/db/Province.java
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -20,6 +20,6 @@ public class Province extends DataSupport
 
     public int getProvinceCode() { return provinceCode; }
 
-    public void setProvinceCode() { this.provinceCode = provinceCode; }
+    public void setProvinceCode(int provinceCode) { this.provinceCode = provinceCode; }
 
 }
